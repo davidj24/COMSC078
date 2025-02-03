2e2a4af872424823a62def84848e371452e55797
davidj24 <tkdave44@gmail.com>
2025-02-02 21:45:26 -0800

    Completed 1st assignment.

---
+++ Assignment1.py
@@ -0,0 +1,4 @@
+mystring = ('My name is david Jo and I am defined by my work ethic and ambitions. But I am currently struggling in my classes right now. I will get back up!')
+
+def introduce(string):
+  print(string)
