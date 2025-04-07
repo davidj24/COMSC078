9af67ad0b10428d6d16002d0f78e2dfc77059038
davidj24 <tkdave44@gmail.com>
2025-04-07 02:04:45 -0700

    Completed Test 2 Program

---
+++ Test 2 Program/Jo_David_Person1.py
@@ -0,0 +1,78 @@
+# David Jo Test 2 Program
+# Program prupose: To implement a Person class and Student subclass to practice object oriented programming and inheritance
+from datetime import date
+
+class Person():
+    def __init__(self, firstName, middleName="", lastName=""):
+        self.firstName = firstName
+        self.middleName = middleName
+        self.lastName = lastName
+
+    def getFirstName(self):
+        return self.firstName
+    
+    def getMiddleName(self):
+        return self.middleName
+    
+    def getLastName(self):
+        return self.lastName
+    
+    def setBirthday(self, birthdate: date):
+        self.birthday = birthdate
+
+    def getAge(self):
+        today = date.today()
+        birthday_passed = (today.month, today.day) < (self.birthday.month, self.birthday.day)
+        age = today.year - self.birthday.year - birthday_passed
+        return age
+    
+    def __str__(self):
+        if self.middleName and self.lastName:
+            return f"{self.firstName} {self.middleName} {self.lastName} is {self.getAge()} years old"
+        if self.middleName == "" and self.lastName:
+            return f"{self.firstName} {self.lastName} is {self.getAge()} years old"
+        if self.middleName and self.lastName == "":
+            return f"{self.firstName} {self.middleName} is {self.getAge()} years old"
+        return f"{self.firstName} is {self.getAge()} years old"
+
+
+class Student(Person):
+
+    next_id = 1
+
+    def __init__(self, firstName, middleName="", lastName="", gender=""):
+        super().__init__(firstName, middleName, lastName)
+        self.gender = gender
+        self.idNumber = Student.next_id
+        Student.next_id += 1
+
+
+    def getidNumber(self):
+        return self.idNumber
+    
+    def __str__(self):
+        return f"{super().__str__()}, and {self.gender} student ID is {self.idNumber}."
+    
+
+
+def main():
+    person1 = Person("Barack", "", "Obama")
+    person1.setBirthday(date(1961, 8, 4))
+
+    person2 = Person("Madonna")
+    person2.setBirthday(date(1958, 8, 16))
+
+    student1 = Student("Abel", "", "Baker", "his")
+    student1.setBirthday(date(2001, 2, 11))
+
+    student2 = Student("Claudette", "", "Davis", "her")
+    student2.setBirthday(date(2002, 5, 28))
+
+    print(person1)
+    print(person2)
+    print(student1)
+    print(student2)
+
+
+if __name__ == "__main__":
+    main()
